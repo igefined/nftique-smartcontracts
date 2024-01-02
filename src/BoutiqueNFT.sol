d9becce9262c7917c48d71cd78342dfefd845005
igefined <ig.pomazkov@gmail.com>
2024-01-02 14:06:31 +0300

    implement release workflow

--- src/BoutiqueNFT.sol
+++ src/BoutiqueNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract BoutiqueNFT is ERC721, ERC721Burnable {
     uint private _nextTokenId = 1;
